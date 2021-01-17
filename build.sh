f553615462bf98f23169f55bb713b4dd04bb778d
James <james@jmablog.com>
2021-01-17 22:48:03 +0000

    Update build for multiple htmls with different names

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pandocomatic -i input -o docs -c web.yaml --modified-only
-
+pandocomatic -i input -o docs -c html.yaml --modified-only
+pandocomatic -i input -o docs -c github.yaml --modified-only
 pandocomatic -i input -o docs -c word.yaml --modified-only
