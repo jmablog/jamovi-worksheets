2e6bc835193d8466cd90b9c2ff90889bf31bab8d
James <james@jmablog.com>
2021-01-19 16:44:55 +0000

    Tidy up

--- templates/shortcodes.lua
+++ templates/shortcodes.lua
@@ -1,54 +1,16 @@
--- if FORMAT:match 'latex' then
---   Str = function (elem)
---     if elem.text == "{{aside}}" then
---       return pandoc.RawInline('latex', '\\begin{tcolorbox}[breakable, colframe=Apricot!40!white, colback=Apricot!10!white, boxsep=2mm]')
---     else if elem.text == "{{end}}" then
---       return pandoc.RawInline('latex', '\\end{tcolorbox}')
---     else
---       return elem
---     end
---   end
--- end
--- else if FORMAT:match 'html' then
---   Str = function (elem)
---     if elem.text == "{{aside}}" then
---       return pandoc.RawInline('html', '<div class="Warning">')
---     else if elem.text == "{{end}}" then
---       return pandoc.RawInline('html', '</div>')
---     else
---       return elem
---     end
---   end
--- end
--- else if FORMAT:match 'docx' then
---   Str = function (elem)
---     if elem.text == "{{aside}}" then
---       return pandoc.Div('', '<div custom-style="Aside">')
---     else if elem.text == "{{end}}" then
---       return pandoc.Div('', '</div>')
---     else
---       return elem
---     end
---   end
--- end
--- end
--- end
--- end
-
 if FORMAT:match 'docx' then
   Div = function (elem)
     if elem.classes[1] == "Aside" then
       elem.attributes['custom-style'] = 'Aside'
       return elem
-    else if elem.classes[1] == "Questions" then
+    elseif elem.classes[1] == "Questions" then
       elem.attributes['custom-style'] = 'QuestionList'
       return elem
     else
       return elem
     end
   end
-end
-else if FORMAT:match 'latex' then
+elseif FORMAT:match 'latex' then
   Div = function (elem)
     if elem.classes[1] == "Aside" then
       return {
@@ -56,7 +18,7 @@ else if FORMAT:match 'latex' then
         elem,
         pandoc.RawBlock('latex', '\\end{tcolorbox}')
       }
-    else if elem.classes[1] == "Questions" then
+    elseif elem.classes[1] == "Questions" then
       return {
         pandoc.RawBlock('latex', '\\begin{tcolorbox}[breakable, colframe=black!50!white, colback=black!3!white, boxsep=2mm]'),
         elem,
@@ -66,6 +28,4 @@ else if FORMAT:match 'latex' then
       return elem
     end
   end
-end
-end
 end
